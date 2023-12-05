a20c3a71a95bca5ec1045416ca1b4f520cbae747
Steve Bedard <stevebedard29@gmail.com>
2023-12-05 16:28:43 -0500

    final game console edition

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var playerScore = 0;
+var cpuScore = 0;
+
 function getComputerChoice(){
     var choices =['Rock', 'Paper', 'Scissors']
     var cpuChoice = choices[Math.floor(Math.random()* choices.length)]
@@ -20,10 +23,13 @@ function playRound (playerSelection, computerSelection){
     if (playerChoice == 'ROCK' && cpuChoice == 'PAPER' ||
         playerChoice == 'PAPER' && cpuChoice == 'ROCK' ||
         playerChoice == 'SCISSORS' && cpuChoice == 'PAPER') {
+            playerScore++;
             return 'You won. ' + playerChoice + ' beats ' + cpuChoice + '.'
+            
         }
 
     else {
+        cpuScore++;
         return 'You lose. ' + cpuChoice + ' beats ' + playerChoice + '.'
     }
 
@@ -31,16 +37,19 @@ function playRound (playerSelection, computerSelection){
 
 function game(){
 
-    let playerScore = 0;
-    let cpuScore = 0;
     let numRounds = 5;
     let computerSelection = '';
+    playerScore = 0;
+    cpuScore = 0;
 
     while(numRounds > 0){
         computerSelection = getComputerChoice();
         var playerChoice = prompt('What is your choice?');
         var result = playRound(playerChoice, computerSelection);
         console.log(result);
+        console.log("The score is:");
+        console.log("Human: " + playerScore);
+        console.log("CPU: " + cpuScore)
         numRounds--;
     }
 }

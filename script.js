76f28c0cb592122ef537d7d19d8cae8890981260
Steve <stevebedard29@gmail.com>
2023-12-05 22:03:42 -0500

    final game with ui

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 var playerScore = 0;
 var cpuScore = 0;
 
-
+const score = document.querySelector('#score');
+const message = document.querySelector('#message');
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
 
     // and for each one we add a 'click' listener
     button.addEventListener('click', () => {
         console.log(button.id);
-        playRound(button.id, getComputerChoice());
+        var result = playRound(button.id, getComputerChoice());
+        message.textContent = result;
+        score.textContent = "Human " + playerScore + " CPU " + cpuScore;
+
+        if (playerScore ==5){
+            message.textContent ="Congratulations you won !";
+            playerScore = 0;
+            cpuScore = 0;
+            
+        }
+
+        if (cpuScore==5){
+            message.textContent ="Sorry you lose!";
+            cpuScore = 0;
+        }
     });
   });
 
@@ -46,6 +61,7 @@ function playRound (playerSelection, computerSelection){
 }
 
 function game(){
+
 
     
 }
